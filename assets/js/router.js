bb8f357b0d1f86b6e64738587d53386e305a7d44
Bilal Ahmed <ba3801534@gmail.com>
2026-01-29 00:30:20 +0500

    completed

--- assets/js/router.js
+++ assets/js/router.js
@@ -1,46 +1,46 @@
-const navLinks = document.querySelectorAll('[data-route]');
-const views = document.querySelectorAll('.app-view');
+// const navLinks = document.querySelectorAll('[data-route]');
+// const views = document.querySelectorAll('.app-view');
 
-function showView(route) {
-  views.forEach(view => view.classList.remove('is-active'));
+// function showView(route) {
+//   views.forEach(view => view.classList.remove('is-active'));
 
-  const target = document.getElementById(route);
-  if (!target) return;
+//   const target = document.getElementById(route);
+//   if (!target) return;
 
-  // reflow to restart animation
-  void target.offsetWidth;
+//   // reflow to restart animation
+//   void target.offsetWidth;
 
-  target.classList.add('is-active');
-}
+//   target.classList.add('is-active');
+// }
 
-function setActiveNav(route) {
-  navLinks.forEach(link => {
-    link.classList.toggle('is-active', link.dataset.route === route);
-  });
-}
+// function setActiveNav(route) {
+//   navLinks.forEach(link => {
+//     link.classList.toggle('is-active', link.dataset.route === route);
+//   });
+// }
 
-navLinks.forEach(link => {
-  link.addEventListener('click', () => {
-    const route = link.dataset.route;
+// navLinks.forEach(link => {
+//   link.addEventListener('click', () => {
+//     const route = link.dataset.route;
 
-    // update URL (no reload)
-    history.pushState({ route }, '', `${route}`);
+//     // update URL (no reload)
+//     history.pushState({ route }, '', `${route}`);
 
-    setActiveNav(route);
-    showView(route);
-  });
-});
+//     setActiveNav(route);
+//     showView(route);
+//   });
+// });
 
-/* Handle browser back / forward */
-window.addEventListener('popstate', () => {
-  const route = location.hash.replace('#', '') || 'home';
-  setActiveNav(route);
-  showView(route);
-});
+// /* Handle browser back / forward */
+// window.addEventListener('popstate', () => {
+//   const route = location.hash.replace('#', '') || 'home';
+//   setActiveNav(route);
+//   showView(route);
+// });
 
-/* Initial load */
-window.addEventListener('load', () => {
-  const route = location.hash.replace('#', '') || 'home';
-  setActiveNav(route);
-  showView(route);
-});
+// /* Initial load */
+// window.addEventListener('load', () => {
+//   const route = location.hash.replace('#', '') || 'home';
+//   setActiveNav(route);
+//   showView(route);
+// });
